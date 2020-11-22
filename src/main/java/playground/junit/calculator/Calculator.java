337b20e6f6cdebbee322a2122c814b616a6f1a47
szykoc <szymon.kociuba@gmail.com>
2020-11-22 13:53:11 +0100

    'komentarze'

--- src/main/java/playground/junit/calculator/Calculator.java
+++ src/main/java/playground/junit/calculator/Calculator.java
@@ -5,5 +5,4 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
-
 }
--- src/test/java/playground/junit/calculator/CalculatorTest.java
+++ src/test/java/playground/junit/calculator/CalculatorTest.java
@@ -2,13 +2,19 @@ package playground.junit.calculator;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-class CalculatorTest {
+class CalculatorTest { // CTRL + SHIFT + T na klasie która chcemy testować
 
-    @Test
-    void addsTwoNumbers() {
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.add(1, 1));
+    @Test // adnotacja bez której nie uruchomimy testu
+    void testAdd(){   // metoda testowa - tutaj testujemy dana funkcjonalnosc
+        //given   sekcja w której przygotowujemy dane potrzebne do rozruchu testu
+        Calculator calc = new Calculator();
+        //when    sekcja w której testujemy daną metode / obiekt (w tym przypadku add())
+        int result = calc.add(5, 10);
+        //then    sekcja sprawdzająca (asercji) weryfikacja czy to co zadziało się
+        //        w sekcji when jest poprawne czyli czy result = 15
+        assertEquals(15, result);
     }
+
 }
