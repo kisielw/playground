ba6e0ac788cd3f81035413f5a70241b5b6127f41
szykoc <szymon.kociuba@gmail.com>
2020-11-22 14:37:35 +0100

    test

--- src/test/java/playground/junit/calculator/CalculatorTest.java
+++ src/test/java/playground/junit/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package playground.junit.calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest { // CTRL + SHIFT + T na klasie która chcemy testować
 
     @Test // adnotacja bez której nie uruchomimy testu
+    @DisplayName("should add two numbers and return result of operation")
     void testAdd(){   // metoda testowa - tutaj testujemy dana funkcjonalnosc
         //given   sekcja w której przygotowujemy dane potrzebne do rozruchu testu
         Calculator calc = new Calculator();
@@ -16,5 +18,48 @@ class CalculatorTest { // CTRL + SHIFT + T na klasie która chcemy testować
         //        w sekcji when jest poprawne czyli czy result = 15
         assertEquals(15, result);
     }
+
+    @Test
+    @DisplayName(" should subtract two numbers and return result")
+    void testSubtract(){
+        //given
+        Calculator calc = new Calculator();
+        //when
+        int result = calc.subtract(1, 1);
+        //then
+        assertEquals(0, result);
+    }
+
+    @Test
+    @DisplayName(" should multiply two numbers and return result")
+    void testMultiply(){
+        //given
+        Calculator calc = new Calculator();
+        //when
+        int result = calc.multiply(5, 5);
+        //then
+        assertEquals(25, result);
+    }
+
+    @Test
+    @DisplayName("")
+    void testDivide(){
+        //given
+        Calculator calculator = new Calculator();
+        //when
+        int result = calculator.divide(20, 2);
+        //then
+        assertEquals(10, result);
+
+        // spojler
+        //ArithmeticException exception =
+        //        assertThrows(ArithmeticException.class,
+        //                    () -> calculator.divide(20, 0));
+
+        //String message = exception.getMessage();
+        //assertEquals("nie dziel przez zero", message);
+    }
+
+
 
 }
